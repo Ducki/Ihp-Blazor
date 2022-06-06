b5c60295ffd242f0afc2efcc61a18641efcd8985
Alexander Wicht <alexwichti@gmail.com>
2022-06-06 18:21:23 +0200

    Fixes ShouldRenderComponentState

--- Ihp-Blazpor.Tests.Unit/Views/Components/FeedComponentTests.cs
+++ Ihp-Blazpor.Tests.Unit/Views/Components/FeedComponentTests.cs
@@ -1,6 +1,8 @@
 using Bunit;
 using FluentAssertions;
+using Ihp_Blazor.Models;
 using Ihp_Blazor.Views.Components;
+using Moq;
 
 namespace Ihp_Blazpor.Tests.Unit.Views.Components;
 
@@ -10,9 +12,10 @@ public class FeedComponentTests : TestContext
     public void ShouldRenderComponentState()
     {
         // Arrange
-
+        var syndicationFeedItem = Mock.Of<LightSyndicationItem>();
         // Act
-        var renderedComponent = RenderComponent<FeedItem>();
+        var renderedComponent = RenderComponent<FeedItem>(builder =>
+            builder.Add(item => item.SyndicationItem, syndicationFeedItem));
 
         // Assert
         renderedComponent.Instance.Should().NotBeNull();
