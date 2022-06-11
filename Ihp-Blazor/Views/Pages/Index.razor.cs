b82435c5e108ad4b387e137374b8f2d451c0e70d
Alexander Wicht <alexwichti@gmail.com>
2022-06-11 16:23:39 +0200

    Adds ILogger

--- Ihp-Blazor/Views/Pages/Index.razor.cs
+++ Ihp-Blazor/Views/Pages/Index.razor.cs
@@ -7,20 +7,21 @@ namespace Ihp_Blazor.Views.Pages;
 public partial class Index : ComponentBase
 {
     [Inject] private IFeedsViewService FeedsViewService { get; set; } = null!;
+    [Inject] private ILogger<Index> Logger { get; set; } = null!;
     private FeedCollectionViewModel? FeedCollectionViewModel { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        Logger.LogInformation("Getting initial feeds");
         FeedCollectionViewModel = await FeedsViewService.GetFeedsViewModel();
     }
 
-    protected override async void OnParametersSet()
+    protected override async void OnAfterRender(bool firstRender)
     {
+        Logger.LogInformation("Getting new feeds after render");
         FeedCollectionViewModel = await FeedsViewService.GetFeedsViewModel();
     }
 
-    private void UpdateTriggered()
-    {
-        Console.WriteLine("TRIGGERED");
-    }
+    private void UpdateTriggered() =>
+        Logger.LogInformation("Triggered update");
 }
