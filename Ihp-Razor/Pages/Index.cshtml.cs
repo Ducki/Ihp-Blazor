783d6df1fa468aba41b3dfa3a6dcb60d1102b3fc
Ducki <alexwichti@live.de>
2023-10-17 20:06:32 +0200

    Adds Razor Pages version

---
+++ Ihp-Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Ihp_Blazor.Models;
+using Ihp_Blazor.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ihp_Razor.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IFeedsService _feedsService;
+    public IEnumerable<LightSyndicationFeed> Feeds { get; set; } = new List<LightSyndicationFeed>();
+
+    public IndexModel(ILogger<IndexModel> logger, IFeedsService feedsService)
+    {
+        _logger = logger;
+        _feedsService = feedsService;
+    }
+
+    public async Task OnGet()
+    {
+        Feeds = await _feedsService.GetFeeds();
+    }
+}
---
+++ Ihp-Razor/Program.cs
@@ -0,0 +1,41 @@
+using Ihp_Blazor.Brokers;
+using Ihp_Blazor.Models;
+using Ihp_Blazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IFeedSourcesBroker>(_ =>
+{
+    var options = new FeedSourcesOptions
+    {
+        FilePath = builder.Configuration.GetValue<string>("FeedSourceFilePath")!
+    };
+
+    return new FeedSourcesBroker(options);
+});
+
+builder.Services.AddScoped<IFeedsService, FeedsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
